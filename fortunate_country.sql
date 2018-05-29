dfe92a2cef591b70ce7cb8cff1df18782ce2c0b3
ivanvatolin <ivanvatolin@gmail.com>
2018-05-29 14:15:31 +0300

    Add files via upload

--- fortunate_country.sql
+++ fortunate_country.sql
@@ -1,15 +1,16 @@
-select name, lang, location, 'best' as sentiment 
-from tweet t1
-inner join (select max([tweet_sentiment]) as tweet_sentiment
-             from tweet) t2 on t1.tweet_sentiment=t2.tweet_sentiment
-inner join user t3 on t1.[user_id]=t3.id
-where location is not null
-union all
-select name, lang, location, 'worst' as sentiment
-from tweet t1
-inner join (select min([tweet_sentiment]) as tweet_sentiment
-             from tweet) t2 on t1.tweet_sentiment=t2.tweet_sentiment
-inner join user t3 on t1.[user_id]=t3.id
-where location is not null;
+# -*- coding: utf-8 -*-
 
-
+   SELECT t1.name,
+          t1.lang,
+          t1.location,
+          t1.tweet_sentiment as sentiment,
+          coalesce(t2.sentiment_desc, t3.sentiment_desc) as sentiment_desc
+     FROM tweet t1
+LEFT JOIN (select max([tweet_sentiment]) as tweet_sentiment, 'best' as sentiment_desc
+             from tweet) t2 on t1.tweet_sentiment = t2.tweet_sentiment
+LEFT JOIN (select min([tweet_sentiment]) as tweet_sentiment, 'worst' as sentiment_desc
+             from tweet) t3 on t1.tweet_sentiment = t3.tweet_sentiment
+    WHERE location IS NOT NULL
+      AND (t2.tweet_sentiment
+               OR t3.tweet_sentiment)
+ ORDER BY t1.tweet_sentiment
